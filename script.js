6b9acc91dc2afc74660e6fad27f5714a68d78ba6
rex-selase1 <rexselasetaylor@gmail.com>
2024-02-10 07:56:23 -0800

    'Finalized'

--- script.js
+++ script.js
@@ -11,21 +11,38 @@ function newQuote() {
   if (!quote.author) {
     authorText.textContent = "Unknown";
   }
+
   quoteText.textContent = quote.text;
-  //   authorText.textContent = quote.author;
+  authorName = quote.author.replace(", type.fit"," ");
+  authorText.textContent = authorName;
+  
 }
 
-async function getQuotes() {
-  const apiUrl = "https://type.fit/api/quotes";
+// async function getQuotes() {
+//   const apiUrl = "https://type.fit/api/quotes";
 
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch (error) {
-    // empty
-  }
-}
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQuotes = await response.json();
+//     newQuote();
+//   } catch (error) {
+//     // empty
+//   }
+// }
+
+getQuotes = async () => {
+  let response = await fetch("https://type.fit/api/quotes");
+  apiQuotes = await response.json();
+  newQuote();
+
+  return apiQuotes;
+};
+
+getQuotes().then(apiQuotes => {
+  apiQuotes = apiQuotes;
+}).catch(err => {
+  return err;
+});
 
 newQuoteBtn.addEventListener("click", newQuote);
 getQuotes();
